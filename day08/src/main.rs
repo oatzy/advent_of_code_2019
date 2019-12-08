b6e4a8bdf5b42c8755d67701d6a385f0b7338a44
Chris Oates <coates@pixitmedia.com>
2019-12-08 18:33:55 +0000

    Day 8, part 2
    I may have gotten carried away with types

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,6 +1,126 @@
 use std::collections::HashMap;
+use std::fmt;
 use std::fs;
+use std::ops::Add;
 use std::u32::MAX;
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+enum Pixel {
+    Black,
+    White,
+    Transparent,
+}
+
+impl From<u32> for Pixel {
+    fn from(value: u32) -> Self {
+        match value {
+            0 => Pixel::Black,
+            1 => Pixel::White,
+            2 => Pixel::Transparent,
+            _ => panic!("unexpected pixel value {}", value),
+        }
+    }
+}
+
+impl Add for Pixel {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        match (self, other) {
+            (p, Pixel::Transparent) => p,
+            (_, p) => p,
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Debug)]
+struct Layer {
+    pixels: Vec<Pixel>,
+}
+
+impl Add for Layer {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Layer {
+            pixels: self
+                .pixels
+                .iter()
+                .zip(other.pixels)
+                .map(|(&x, y)| x + y)
+                .collect(),
+        }
+    }
+}
+
+struct Image {
+    layers: Vec<Layer>,
+    width: u32,
+    height: u32,
+}
+
+impl Image {
+    fn from_raw(pixels: &Vec<u32>, width: u32, height: u32) -> Self {
+        let pixels = &pixels[..];
+
+        let layer_size = (width * height) as usize;
+        let layers = pixels
+            .chunks(layer_size)
+            .map(|layer| Layer {
+                pixels: layer.iter().map(|&x| Pixel::from(x)).collect(),
+            })
+            .collect();
+
+        Image {
+            layers: layers,
+            width: width,
+            height: height,
+        }
+    }
+
+    fn flatten(self) -> FlatImage {
+        // reverse because we want to merge bottom-up
+        let mut layers = self.layers.into_iter().rev();
+        let base = layers.next().unwrap();
+        let flat_layer = layers.fold(base, |acc, x| acc + x);
+
+        FlatImage {
+            pixels: flat_layer.pixels,
+            width: self.width,
+            height: self.height,
+        }
+    }
+}
+
+struct FlatImage {
+    pixels: Vec<Pixel>,
+    width: u32,
+    #[allow(dead_code)]
+    height: u32,
+}
+
+impl fmt::Display for FlatImage {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let pixels = &self.pixels[..];
+        for row in pixels.chunks(self.width as usize) {
+            write!(
+                f,
+                "{}\n",
+                row.iter()
+                    .map(|x| {
+                        match x {
+                            Pixel::Black => "█".to_string(),
+                            Pixel::White => " ".to_string(),
+                            Pixel::Transparent => panic!("got unexpecte transparent pixel"),
+                        }
+                    })
+                    .collect::<Vec<String>>()
+                    .join("")
+            )?
+        }
+        Ok(())
+    }
+}
 
 fn checksum(pixels: &Vec<u32>, width: usize, height: usize) -> u32 {
     let pixels = &pixels[..];
@@ -38,4 +158,68 @@ fn main() {
 
     let part1 = checksum(&input, width, height);
     println!("{}", part1);
+
+    let image = Image::from_raw(&input, width as u32, height as u32);
+    let image = image.flatten();
+    print!("{}", image);
+}
+
+mod test {
+    #[test]
+    fn test_add_pixels() {
+        use super::Pixel;
+
+        assert_eq!(Pixel::Black + Pixel::White, Pixel::White);
+        assert_eq!(Pixel::White + Pixel::Black, Pixel::Black);
+        assert_eq!(Pixel::Black + Pixel::Transparent, Pixel::Black);
+        assert_eq!(Pixel::White + Pixel::Transparent, Pixel::White);
+        assert_eq!(Pixel::Transparent + Pixel::Transparent, Pixel::Transparent);
+    }
+
+    #[test]
+    fn test_add_layer() {
+        use super::{Layer, Pixel};
+
+        let lower = Layer {
+            pixels: vec![
+                Pixel::Black,
+                Pixel::Transparent,
+                Pixel::Transparent,
+                Pixel::Transparent,
+            ],
+        };
+        let upper = Layer {
+            pixels: vec![
+                Pixel::White,
+                Pixel::White,
+                Pixel::Transparent,
+                Pixel::Transparent,
+            ],
+        };
+
+        assert_eq!(
+            lower + upper,
+            Layer {
+                pixels: vec![
+                    Pixel::White,
+                    Pixel::White,
+                    Pixel::Transparent,
+                    Pixel::Transparent
+                ]
+            }
+        );
+    }
+
+    #[test]
+    fn test_flatten() {
+        use super::{Image, Pixel};
+
+        let image = Image::from_raw(&vec![0, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 2, 0, 0, 0, 0], 2, 2);
+        let image = image.flatten();
+
+        assert_eq!(
+            image.pixels,
+            vec![Pixel::Black, Pixel::White, Pixel::White, Pixel::Black]
+        );
+    }
 }
