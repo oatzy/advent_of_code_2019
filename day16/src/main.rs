08ceed60bf5bb7240fc0e90568b9366e50390a5d
Chris Oates <kryz.oatz@gmail.com>
2019-12-17 00:17:49 +0000

    wip: day 16, part 2
    Still trying to figure it out. That O(n2) on ~5m values is a killer.

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -3,54 +3,117 @@ use std::iter;
 
 type V = Vec<isize>;
 
-fn FFT(signal: &V, pattern: &V) -> V {
+fn fft_digit(signal: &V, pattern: &V, digit: usize) -> isize {
+    signal
+        .iter()
+        .zip(
+            pattern
+                .iter()
+                .map(|x| iter::repeat(x).take(digit + 1))
+                .flatten()
+                .cycle()
+                .skip(1),
+        )
+        .skip(digit)
+        .map(|(a, b)| (a * b))
+        .sum::<isize>()
+        .abs()
+        % 10
+}
+
+fn fft_digit_fn(signal: &V, digit: usize) -> isize {
+    // slightly faster than the nested iterators
     signal
         .iter()
         .enumerate()
-        .map(|(i, _)| {
-            signal
-                .iter()
-                .zip(
-                    pattern
-                        .iter()
-                        .map(|x| iter::repeat(x).take(i + 1))
-                        .flatten()
-                        .cycle()
-                        .skip(1),
-                )
-                .map(|(a, b)| (a * b))
-                .sum()
+        .map(|(n, &x)| match ((n + 1) / (digit + 1)) % 4 {
+            1 => x,
+            3 => -x,
+            _ => 0,
         })
-        .map(|x: isize| x.abs() % 10)
+        .sum::<isize>()
+        .abs()
+        % 10
+}
+
+fn FFT(signal: &V, pattern: &V) -> V {
+    (0..signal.len())
+        //.map(|i| fft_digit(&signal, &pattern, i))
+        .map(|i| fft_digit_fn(&signal, i))
         .collect()
 }
 
 fn fft_phases(signal: &V, pattern: &V, loops: usize) -> V {
     let mut output = signal.clone();
-    for _ in 0..loops {
+    for i in 0..loops {
         output = FFT(&output, &pattern);
+        println!("{}", i);
+    }
+    output
+}
+
+fn fft_from(signal: &V, pattern: &V, start: usize) -> V {
+    (0..signal.len())
+        .map(|i| fft_digit_fn(&signal, i + start))
+        .collect()
+}
+
+fn fft_phases_from(signal: &V, pattern: &V, loops: usize, start: usize) -> V {
+    println!("cloning input");
+    let mut output = signal.clone();
+    println!("looping");
+    for i in 0..loops {
+        output = fft_from(&output, &pattern, start);
+        println!("{}", i);
     }
     output
 }
 
 fn main() {
     let input = fs::read_to_string("/home/chris/advent_of_code/2019/inputs/day16.txt").unwrap();
+
+    let pattern = vec![0, 1, 0, -1];
+
+    // let input: V = input
+    //     .trim()
+    //     .chars()
+    //     .map(|x| x.to_digit(10).unwrap() as isize)
+    //     .collect();
+
+    // let part1 = fft_phases(&input, &pattern, 100)
+    //     .iter()
+    //     .take(8)
+    //     .map(|&x| x)
+    //     .map(|x| x.to_string())
+    //     .collect::<Vec<String>>()
+    //     .join("");
+
+    // println!("{}", part1);
+
+    let start: usize = input[..7].parse().unwrap();
+    let target_length = 10_000 * input.len();
+
+    println!("{} - {}", start, target_length);
+
+    println!("building input");
     let input: V = input
         .trim()
         .chars()
         .map(|x| x.to_digit(10).unwrap() as isize)
+        .cycle()
+        .skip(start)
+        .take(target_length - start)
         .collect();
-    let pattern = vec![0, 1, 0, -1];
 
-    let part1 = fft_phases(&input, &pattern, 100)
+    println!("performing transform");
+    let part2 = fft_phases_from(&input, &pattern, 100, start)
         .iter()
         .take(8)
-        .map(|&x| x)
-        .map(|x| x.to_string())
+        .map(|&x| x.to_string())
         .collect::<Vec<String>>()
         .join("");
 
-    println!("{}", part1);
+    println!("{}", part2);
 }
 
 #[cfg(test)]
